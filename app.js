f57a19c47d785aff276831c213a7a37a8a18b045
Jinnie Cho <yejincho1105@gmail.com>
2022-01-29 18:26:09 +0900

    🔨 Added click events to edit and delete buttons

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// #### step 1: 내용 추가
+// #### TODO 내용 추가
 
 // - [x] input에 내용을 입력하고 확인 버튼을 누르면 list에 내용이 보인다
 // - [x] input에 내용을 입력하고 앤터키를 누르면 list에 내용이 보인다
@@ -10,23 +10,26 @@
 // - [x] 내용이 추가되고 나면 input은 빈 값으로 초기화한다.
 // - [x] 사용자 입력값이 빈 값이라면 추가 되지 않는다.
 
-// #### step 2: 내용 수정
+// #### TODO 내용 수정
 
-// - [ ] 내용의 수정 버튼을 클릭 이벤트를 받으면, 내용을 수정하는 prompt가 뜬다.
-// - [ ] prompt에서 신규 내용을 입력 받고, 확인 버튼을 누르면 내용이 수정된다.
+// - [x] 내용의 수정 버튼을 클릭 이벤트를 받으면, 내용을 수정하는 prompt가 뜬다.
+// - [x] prompt에서 신규 내용을 입력 받고, 확인 버튼을 누르면 내용이 수정된다.
+// - [x] 내용의 수정 버튼을 클릭 이벤트를 받으면, 내용을 수정하는 prompt가 뜬다.
 
-// #### step 3: 내용 삭제
+// #### TODO 내용 삭제
 
-// - [ ] 내용 삭제 버튼 클릭 이빈트를 받고, 메뉴 삭제 컨펌 prompt가 뜬다.
-// - [ ] 확인 버튼을 클릭하면 내용이 삭제 된다.
-// - [ ] 총 내용 갯수를 count하여 상단에 보여준다.
+// - [x] 내용 삭제 버튼을 클릭하면 클릭 이벤트가 실행되고, 메뉴 삭제 컨펌(confirm)가 뜬다.
+// - [x] 확인 버튼을 클릭하면 내용이 삭제 된다.
+// - [x] 총 내용 갯수를 count하여 상단에 보여준다.
 
 const $ = (selector) => document.querySelector(selector);
 
 function App() {
-  $('#list-form').addEventListener('submit', (e) => {
-    e.preventDefault();
-  });
+  const updateListCounter = () => {
+    const listCounter = $('.list-group').querySelectorAll('li').length;
+    $('.badge-title-list-counter').innerText = `total: ${listCounter}`;
+    $('.badge-title-list-counter').ariaLabel = `you have ${listCounter} lists`;
+  };
 
   const addListcontent = () => {
     if ($('#emoji-off-input-text').value === '') {
@@ -36,42 +39,32 @@ function App() {
 
     const emojiOffInputText = $('#emoji-off-input-text').value;
     const listItemTemplate = (listText) => {
-      return `<li class="list-item is-more">
+      return `<li class="list-item">
                     <div class="list-content">
                       <p class="list-text">${listText}</p>
                     </div>
 
                     <div class="badge-group">
                       <div class="badge badge-sm sm-only">
-                        <button class="badge badge-sm-more" type="button">
-                          <i class="ic-more"></i>
-                        </button>
-                        <div class="badge-sm-hidden">
                           <button
-                            class="badge badge-sm-hidden-arrow-back"
-                            type="button"
-                          >
-                            <i class="ic-arrow-back"></i>
-                          </button>
-                          <button
-                            class="badge badge-sm-hidden-edit"
+                            class="badge badge-sm-edit"
                             type="button"
                           >
                             <i class="ic-edit"></i>
                           </button>
                           <button
-                            class="badge badge-sm-hidden-archive"
+                            class="badge badge-sm-archive"
                             type="button"
                           >
                             <i class="ic-save"></i>
                           </button>
                           <button
-                            class="badge badge-sm-hidden-delete"
+                            class="badge badge-sm-delete"
                             type="button"
                           >
                             <i class="ic-rubbish"></i>
                           </button>
-                        </div>
+                        
                       </div>
 
                       <div class="badge badge-md sm-hidden">
@@ -94,9 +87,7 @@ function App() {
       listItemTemplate(emojiOffInputText)
     );
 
-    const listCounter = $('.list-group').querySelectorAll('li').length;
-    $('.badge-title-list-counter').innerText = `total: ${listCounter}`;
-    $('.badge-title-list-counter').ariaLabel = `you have ${listCounter} lists`;
+    updateListCounter();
 
     if (listCounter >= 99) {
       $('.badge-title-list-counter').innerText = `total: 99+`;
@@ -104,6 +95,38 @@ function App() {
 
     $('#emoji-off-input-text').value = '';
   };
+
+  $('.list-group').addEventListener('click', (e) => {
+    if (
+      e.target.classList.contains('badge-md-edit') ||
+      e.target.classList.contains('badge-sm-edit')
+    ) {
+      const listText = e.target.closest('li').querySelector('.list-text');
+      const editedListText = prompt(
+        'how would you liked to edit?',
+        listText.innerText
+      );
+      listText.innerText = editedListText;
+
+      if (editedListText === null) {
+        return;
+      }
+    }
+
+    if (
+      e.target.classList.contains('badge-md-delete') ||
+      e.target.classList.contains('badge-sm-delete')
+    ) {
+      if (confirm('are you sure deleting this one?')) {
+        e.target.closest('li').remove();
+        updateListCounter();
+      }
+    }
+  });
+
+  $('#list-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+  });
 
   $('.btn-input-add').addEventListener('click', () => {
     addListcontent();
